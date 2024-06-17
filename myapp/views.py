80c5478544ec15d79804cc214c4ab7478c021b8c
sanderlight <116835755+sanderlight@users.noreply.github.com>
2024-06-17 18:35:58 -0400

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -35,7 +35,7 @@ from dotenv import load_dotenv
 
 
 
-openai.api_key = "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"
+openai.api_key = "sk-proj-qCGzDg6SjyLjXZSH4rJfT3BlbkFJe1YfzIzeZl6INdXw4cJr"
 
 
 def user_login(request):
