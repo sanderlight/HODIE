05b13a94f232f3c22213c98aac5ed1a37a3135e0
sanderlight <116835755+sanderlight@users.noreply.github.com>
2024-06-17 01:32:22 -0400

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -30,7 +30,7 @@ import uuid
 
 
 
-openai.api_key = sk-proj-JkrH468YCZT6q6OO2UQPT3BlbkFJz4KiCiOjiyOUth79HwKJ
+openai.api_key = "sk-proj-JkrH468YCZT6q6OO2UQPT3BlbkFJz4KiCiOjiyOUth79HwKJ"
 
 
 def user_login(request):
